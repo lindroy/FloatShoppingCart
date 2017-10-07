30d40804a5df056c8cdac8e035daa127b6578a1b
Lindroid <linyulong2014@163.com>
2017-10-07 12:03:40 +0800

    计算控件的宽度和移动距离，并实现了显示和隐藏的动画效果。

--- app/src/main/java/com/lindroid/floatshoppingcart/MainActivity.java
+++ app/src/main/java/com/lindroid/floatshoppingcart/MainActivity.java
@@ -1,7 +1,18 @@
 package com.lindroid.floatshoppingcart;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ViewTreeObserver;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -13,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ImageView ivCart;
     private List<String> titles = new ArrayList<>();
+    private boolean isShowFloatImage = true;
+    private float startY;
+    private int moveDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +45,112 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         listView = (ListView) findViewById(R.id.listView);
         ivCart = (ImageView) findViewById(R.id.iv_cart);
-        listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,titles));
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+
+        ivCart.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Log.d("Tag", "控件宽度" + ivCart.getWidth());
+                moveDistance = getDisplayMetrics(MainActivity.this)[0] - ivCart.getRight() + ivCart.getWidth() / 2;
+                //监听结束之后移除监听事件
+                ivCart.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (Math.abs(startY - event.getY()) > 10) {
+                    hideFloatImage(moveDistance);
+                }
+                startY = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                new Handler(new Handler.Callback() {
+                    @Override
+                    public boolean handleMessage(Message msg) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                showFloatImage(moveDistance);
+                            }
+                        });
+                        return false;
+                    }
+                }).sendEmptyMessageDelayed(0, 1500);
+
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+
+    /**
+     * 显示悬浮图标
+     *
+     * @param distance
+     */
+    private void showFloatImage(int distance) {
+        isShowFloatImage = false;
+        //位移动画
+        TranslateAnimation ta = new TranslateAnimation(
+                distance,//起始x坐标
+                0,//结束x坐标
+                0,//起始y坐标
+                0);//结束y坐标（正数向下移动）
+        ta.setDuration(300);
+
+        //渐变动画
+        AlphaAnimation al = new AlphaAnimation(0.5f, 1f);
+        al.setDuration(300);
+
+        AnimationSet set = new AnimationSet(true);
+        //动画完成后不回到原位
+        set.setFillAfter(true);
+        set.addAnimation(ta);
+        set.addAnimation(al);
+        ivCart.startAnimation(set);
+    }
+
+    /**
+     * 隐藏悬浮图标
+     *
+     * @param distance
+     */
+    private void hideFloatImage(int distance) {
+        isShowFloatImage = false;
+        //位移动画
+        TranslateAnimation ta = new TranslateAnimation(
+                0,//起始x坐标,10表示与初始位置相距10
+                distance,//结束x坐标
+                0,//起始y坐标
+                0);//结束y坐标（正数向下移动）
+        ta.setDuration(300);
+
+        //渐变动画
+        AlphaAnimation al = new AlphaAnimation(1f, 0.5f);
+        al.setDuration(300);
+
+        AnimationSet set = new AnimationSet(true);
+        //动画完成后不回到原位
+        set.setFillAfter(true);
+        set.addAnimation(ta);
+        set.addAnimation(al);
+        ivCart.startAnimation(set);
+
+    }
+
+    private int[] getDisplayMetrics(Context context) {
+        DisplayMetrics mDisplayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(mDisplayMetrics);
+        int W = mDisplayMetrics.widthPixels;
+        int H = mDisplayMetrics.heightPixels;
+        int array[] = {W, H};
+        return array;
     }
 }
